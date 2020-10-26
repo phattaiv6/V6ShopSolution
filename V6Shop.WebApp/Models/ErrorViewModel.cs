1b75191a17bc27f866b218ea03c718e4bc97e256
phattaiv6 <luonghuynhphattai@gmail.com>
2020-10-26 15:50:13 +0700

    Add project files.

---
+++ V6Shop.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace V6Shop.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
